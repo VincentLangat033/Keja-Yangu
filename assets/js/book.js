64371379a05fd57f1feab5db1d4cb6609db52446
MarkBrianK <markbrian.karanja@student.moringaschoool.com>
2022-05-18 15:54:47 +0300

    changes

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,4 +1,10 @@
-function saveUserData() {
-    var userDAta = document.getElementById("userData");
-
+let data = [];
+const addData = (eV) => {
+    eV.preventDefault();
+    let data ={
+        document.getElementById("input").value
+    }
 }
+document.addEventListener('DOMContentLoaded',()=>{
+     document.getElementById('btn').addEventListener('click', addData);
+ });
