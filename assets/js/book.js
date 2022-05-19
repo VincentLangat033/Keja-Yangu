f1aab50db440e817040e98d18a0b9f6a18139b57
MarkBrian <104422992+MarkBrianK@users.noreply.github.com>
2022-05-19 11:18:59 +0300

    Update book.js
    Resolving conflicts on this file

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
 
-=======
-let data = [];
-const addData = (eV) => {
-    eV.preventDefault();
-    let data ={
-        document.getElementById("input").value
-    }
-}
-document.addEventListener('DOMContentLoaded',()=>{
-     document.getElementById('btn').addEventListener('click', addData);
- });
->>>>>>> 64371379a05fd57f1feab5db1d4cb6609db52446
