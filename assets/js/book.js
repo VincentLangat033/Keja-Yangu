3352a24d78ffdb05286025ddf0350c03070e7f24
MarkBrianK <markbrian.karanja@student.moringaschoool.com>
2022-05-19 09:33:54 +0300

    I have changed the HTML

---
+++ assets/js/book.js
@@ -0,0 +1,7 @@
+function saveData(){
+    alert("Your data has been sbmitted succesfully")
+}
+// function saveData1(){
+//     document.getElementById("btn")
+//     saveData1.innerHtml(" Your data has been saved")
+// }
