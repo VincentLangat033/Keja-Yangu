519862d25066325a54828704a7b7d507ebe9eef4
MarkBrianK <markbrian.karanja@student.moringaschoool.com>
2022-05-19 10:55:17 +0300

    Changes on html

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
-function saveData(){
-    alert("Your data has been sbmitted succesfully")
-}
-// function saveData1(){
-//     document.getElementById("btn")
-//     saveData1.innerHtml(" Your data has been saved")
-// }
-=======
-function saveUserData() {
-    var userDAta = document.getElementById("userData");
 
-}
->>>>>>> 9c013b00ad138883467c9c0404c7199652992118
